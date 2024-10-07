b4761a204d4e5927cc938026a6d6ef6f897831be
arulgundam <arul.gundam@gmail.com>
2024-10-07 13:40:32 -0400

    done

---
+++ unit-converter/unit-converter/unit_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  unit_converterApp.swift
+//  unit-converter
+//
+//  Created by Arul Gundam on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct unit_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
